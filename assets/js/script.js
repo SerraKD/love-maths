696ae2cf9ab04003f589811786ff1495e0b5f649
Serra KIR DWORCZAK <serrakir@gmail.com>
2024-02-27 08:48:23 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,8 +30,10 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    } else if(gameType === "multiply") {
+    } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1,num2);
+    } else if (gameType === "subtract") { 
+        displaySubtractQuestion(num1,num2);
     } else {
         alert(`Unkown game type: ${gameType}`);
         throw(`Unkown game type: ${gameType}. Aborting!`);
@@ -74,6 +76,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") { 
         return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     } else {
         alert(`Unipmlemented operator: ${operator}`);
         throw(`Unipmlemented operator: ${operator}. Aborting!`);
@@ -109,7 +113,11 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
 
 }
 
